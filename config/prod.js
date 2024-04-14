04c75ba48739a78ff985c1d46f335bcefc4e335e
Indu <wjsdlsdn1158@naver.com>
2024-04-14 16:13:23 +0900

    비밀 번호 보호 설정 관리

---
+++ config/prod.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI : process.env.MONGO_URI
+} // heroku에서 설정한 이름과 동일해야 함
--- index.js
+++ index.js
@@ -1,17 +1,40 @@
 const express = require('express');
 const app = express();
 const port = 5000;
+const bodyParser = require('body-parser');
+const { User } = require("./models/Users");
+const config = require('./config/key');
+
+//application/x-www-form-urlencoded 형식의 데이터를 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json 형식의 데이터를 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://Indwoo:0504@cluster0.5zredre.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-}).then(() => console.log('MongoDB Connected...'))
+mongoose.connect(config.mongoURI, {}).then(() => console.log('MongoDB Connected...'))
     .catch(err => console.log(err));
 
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!!!!'));
+
+
+app.post('/register',(req,res)=>{   
+    //회원가입할 때 필요한 정보들을 client에서 가져오면,
+    //그 정보들을 DB에 넣어준다.
+    const user = new User(req.body);
+
+    //user모델에 정보가 저장됨
+    //실패 시, 실패한 정보를 보내줌
+    user.save().then(()=>{
+        res.status(200).json({
+            success:true
+        })
+    }).catch((err)=>{
+        return res.json({success:false,err})
+    });
+
+
+})
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
